d065e425036e177ee3537adbdc17669d3e48c1a8
EkanshSin23 <90972216+EkanshSin23@users.noreply.github.com>
2022-07-17 20:59:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
         <Route exact path="/about" element={<About />} />
         <Route exact path="/contact" element={<Contact />} />
         <Route exact path="/service" element={<Service />} />
+           <Route path="*" element={<Home />} />
 
       </Routes>
 
